ecefecdae6ff366feb4fbac11141cbc30187657b
J.P <jpzambrano95@hotmail.com>
2019-06-16 23:03:02 -0500

    Created date-picker component
     - Moved code, that creates a custom date-picker, to its own file.
        * gave it class binding for displaying as inline-block.
     - Made text-field's value bound to 'workoutTitle' property.

---
+++ src/components/workout-log/date-picker.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-menu
+    ref="menu"
+    v-model="menu"
+    :close-on-content-click="false"
+    :nudge-right="40"
+    :return-value.sync="date"
+    lazy
+    transition="scale-transition"
+    offset-y
+    min-width="120px"
+  >
+    <template v-slot:activator="{ on }">
+      <v-text-field
+        v-model="date"
+        readonly
+        box
+        :class="{ 'd-inline-block': isInlineBlock }"
+        v-on="on"
+      />
+    </template>
+    <v-date-picker v-model="date" no-title scrollable>
+      <v-spacer />
+      <v-btn flat color="primary" @click="menu = false">
+        Cancel
+      </v-btn>
+      <v-btn flat color="primary" @click="$refs.menu.save(date)">
+        OK
+      </v-btn>
+    </v-date-picker>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'DatePicker',
+
+  props: {
+    isInlineBlock: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data: () => ({
+    date: new Date().toISOString().substr(0, 10),
+    menu: false
+  })
+}
+</script>
--- src/features/home/home.vue
+++ src/features/home/home.vue
@@ -1,52 +1,31 @@
 <template>
   <v-layout row wrap>
-    <v-flex xs12 sm6 md4>
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :nudge-right="40"
-        :return-value.sync="date"
-        lazy
-        transition="scale-transition"
-        offset-y
-        full-width
-        min-width="290px"
-      >
-        <template v-slot:activator="{ on }">
-          <v-text-field
-            v-model="date"
-            label="Picker in menu"
-            prepend-icon="event"
-            readonly
-            v-on="on"
-          />
-        </template>
-        <v-date-picker v-model="date" no-title scrollable>
-          <v-spacer />
-          <v-btn flat color="primary" @click="menu = false">
-            Cancel
-          </v-btn>
-          <v-btn flat color="primary" @click="$refs.menu.save(date)">
-            OK
-          </v-btn>
-        </v-date-picker>
-      </v-menu>
+    <v-flex class="log__title" xs6>
+      <v-text-field :value="workoutTitle" class="d-inline-block" box></v-text-field>
+      <date-picker is-inline-block></date-picker>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import DatePicker from '@/components/workout-log/date-picker'
+
 export default {
+  components: {
+    DatePicker
+  },
+
   data: () => ({
-    date: new Date().toISOString().substr(0, 10),
-    menu: false
+    workoutTitle: 'Workout 1'
   })
 }
 </script>
 
 <style lang="stylus">
 #app
-  .o-home
-    height: 280px
+  .log
+    &__title
+      .v-text-field
+        padding: 0
+        margin: 0
 </style>
