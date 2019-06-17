de9f657b54eb318e35bffcbbb519c7817f4e732a
J.P <jpzambrano95@hotmail.com>
2019-06-16 20:11:40 -0500

    Added default route that goes to home page.
     - It was annoying having to type /home in the url.
     - Found reference to boilerplate app. Updated to hogweights.

--- src/http/routes.js
+++ src/http/routes.js
@@ -4,6 +4,14 @@
  */
 const routes = [
   {
+    name: 'default',
+    path: '',
+    component: () => import(/* webpackChunkName: "default" */ '@/features/home/home.vue'),
+    title: 'Home',
+    layout: 'DefaultLayout',
+    isPublic: true
+  },
+  {
     name: 'home',
     path: '/home',
     component: () => import(/* webpackChunkName: "home" */ '@/features/home/home.vue'),
